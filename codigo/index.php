292f63082e9596803ed81e7aacf4ce0540ee2c24
Luana321 <luana.morais1@estudante.ifgoiano.edu.br>
2026-09-24 08:16:42 -0300

    codigos

--- codigo/index.php
+++ codigo/index.php
@@ -3,17 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-     <style>
-        .erro {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <?php
+<?php
         if (isset($_GET['erro'])) {
             $email = '';
             if ($_GET['erro'] == "login") {
@@ -27,14 +21,21 @@
         else {
             $email = "";
         }
-    ?>
-    <form action="verificar_login.php" method="post">
-        E-mail: <br>
-        <input type="text" name="email" value="<?php echo $email; ?>"> <br><br>
-        Senha: <br>
-        <input type="text" name="senha"> <br><br>
+?>
+<form action="verificar_login.php" method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" class="form-control" name="email" aria-describedby="emailHelp" value="<?php echo $email; ?>">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Senha</label>
+    <input type="password" class="form-control" name="senha">
+  </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 
-        <input type="submit" value="Entrar">
-    </form>
+  <p>Não tem uma conta? <a href="cadastroUsuario.php">Cadastre-se</a></p>
+</form>
 </body>
 </html>
